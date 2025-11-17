396474d883618c7971283846bb000be7d9f67f7c
asanjaye <sanjaye@usc.edu>
2025-11-17 12:14:42 -0800

    API endpoint connected

--- security/src/main/java/com/bball/security/config/SecurityConfiguration.java
+++ security/src/main/java/com/bball/security/config/SecurityConfiguration.java
@@ -24,7 +24,7 @@ public class SecurityConfiguration {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/v1/auth/**").permitAll()
+                        .requestMatchers("/api/v1/auth/register", "/api/v1/auth/authenticate").permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session
