70b77e7d5c7529e65b4fa5152bcb96b3a1ad8bab
Rushil Shah <rushilrs@usc.edu>
2025-12-10 17:08:20 -0800

    configured observability

--- security/src/main/java/com/bball/security/config/SecurityConfiguration.java
+++ security/src/main/java/com/bball/security/config/SecurityConfiguration.java
@@ -24,7 +24,7 @@ public class SecurityConfiguration {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/v1/auth/register", "/api/v1/auth/authenticate").permitAll()
+                        .requestMatchers("/api/v1/auth/register", "/api/v1/auth/authenticate", "/actuator/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session
